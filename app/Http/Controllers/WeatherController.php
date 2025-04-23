3ddf8932d81b36c9b65dbf926a53f86b92617d05
Isiji <blairisiji@gmail.com>
2025-04-23 15:31:57 +0300

    fetching more information from the weather API

--- app/Http/Controllers/WeatherController.php
+++ app/Http/Controllers/WeatherController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Carbon;
 
 class WeatherController extends Controller
 {
@@ -12,28 +13,62 @@ class WeatherController extends Controller
         $lat = $request->query('lat');
         $lon = $request->query('lon');
         $units = $request->query('units', 'metric'); // default to metric
+        $city = $request->query('city');
         $apiKey = env('OPENWEATHERMAP_API_KEY');
 
-        \Log::info("Using API key: $apiKey"); 
+        \Log::info("Using API key: $apiKey");
 
         if (!$lat || !$lon) {
             return response()->json(['error' => 'Missing coordinates'], 400);
         }
 
-        $response = Http::get("https://api.openweathermap.org/data/2.5/weather", [
+        // Current weather
+        $currentResponse = Http::get("https://api.openweathermap.org/data/2.5/weather", [
             'lat' => $lat,
             'lon' => $lon,
             'appid' => $apiKey,
             'units' => $units
         ]);
+        $currentData = $currentResponse->json();
 
-        $data = $response->json();
+        // Forecast
+        $forecastResponse = Http::get("https://api.openweathermap.org/data/2.5/forecast", [
+            'lat' => $lat,
+            'lon' => $lon,
+            'appid' => $apiKey,
+            'units' => $units
+        ]);
+        $forecastData = $forecastResponse->json();
+
+        $forecastDays = [];
+        $seenDays = [];
+
+        foreach ($forecastData['list'] as $entry) {
+            $date = Carbon::parse($entry['dt_txt']);
+            $dayName = $date->format('l');
+
+            if (!in_array($dayName, $seenDays) && $date->hour === 12) {
+                $forecastDays[] = [
+                    'day' => $dayName,
+                    'temp' => round($entry['main']['temp']),
+                    'icon' => $entry['weather'][0]['icon'],
+                ];
+                $seenDays[] = $dayName;
+            }
+
+            if (count($forecastDays) >= 3) break;
+        }
 
         return response()->json([
-            'weather' => ucfirst($data['weather'][0]['description']),
-            'city' => $data['name'],
-            'units' => $request->query('units', 'metric')
+            'weather' => ucfirst($currentData['weather'][0]['description']),
+            'icon' => $currentData['weather'][0]['icon'],
+            'city' => $city ?? $currentData['name'],
+            'units' => $units,
+            'temperature' => round($currentData['main']['temp']),
+            'date' => now()->toDateString(),
+            'windSpeed' => $currentData['wind']['speed'],
+            'humidity' => $currentData['main']['humidity'],
+            'forecast' => $forecastDays,
         ]);
-
     }
 }
